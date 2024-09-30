c4b7092b8d8fcca5552c9c31de97221627aa2ef8
Sparrowave Tech <105904440+sparrowaves@users.noreply.github.com>
2024-09-30 13:25:42 +0530

    removed plugin (Ignore)

--- platform/plugins/fob-turnstile/config/permissions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    [
-        'name' => 'Turnstile',
-        'flag' => 'turnstile.settings',
-    ],
-];
--- platform/plugins/fob-turnstile/resources/views/forms/fields/turnstile.blade.php
+++
@@ -1,5 +0,0 @@
-@php
-    $id = 'cf-turnstile-' . hash('sha256', time() . rand())
-@endphp
-
-<div class="mb-3 cf-turnstile" id="{{ $id }}"></div>
