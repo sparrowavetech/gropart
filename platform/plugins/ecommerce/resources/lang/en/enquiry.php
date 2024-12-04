d467fc0582261159d2411f3f01e6f6db6978af2d
Sparrowave Tech <105904440+sparrowaves@users.noreply.github.com>
2024-12-05 02:11:02 +0530

    Enquiry Detail in admin Fixed

--- platform/plugins/ecommerce/resources/lang/en/enquiry.php
+++ platform/plugins/ecommerce/resources/lang/en/enquiry.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'name'                  => 'Enquiry',
-    'intro'                 => [
+    'name'              => 'Enquiry',
+    'intro'             => [
         'title'       => 'Manage Enquiry',
         'description' => 'When a customer submit your enquiry(s), you will know their enquiry histories.',
     ],
@@ -14,13 +14,22 @@ return [
     ],
     'statuses'              => [
         'pending' => 'Pending',
+        'reject' => 'Reject',
         'contacted'    => 'Enquiry Contacted',
         'not_available' => 'Not Available',
         'rejected' => 'Rejected'
     ],
+    'status' => 'Status',
+    'download_attachment' => 'Download Attachment File',
+    'customer_name' => 'Customer Name',
+    'customer_email' => 'Customer Email',
+    'customer_phone' => 'Customer Mobile Number',
+    'customer_address' => 'Customer Address',
+    'customer_attachment' => 'Attachment File',
+    'customer_product' => 'Product Name',
+    'customer_store' => 'Product Store',
     'edit'  => 'View Enquiry :code',
     'reject'  => 'Enquiry Rejected',
-    'information' => 'Enquiry information',
+    'enquiry_information' => 'Enquiry information',
     'status'   => 'Enquiry Status'
-
 ];
--- platform/plugins/ecommerce/resources/views/enquires/edit.blade.php
+++ platform/plugins/ecommerce/resources/views/enquires/edit.blade.php
@@ -1,118 +1,151 @@
 @extends(BaseHelper::getAdminMasterLayoutTemplate())
 
 @section('content')
-<div id="main-order-content">
-    <div class="col-md-12">
-        <div class="card mb-3">
-            <div class="card-title">
-                Enquiry information #GPO-10000326
-            </div>
-            <div class="card-body">
-                @if ($enquiry->status == \Botble\Ecommerce\Enums\EnquiryStatusEnum::REJECT)
-                    @php
-                        $class= "danger";
-                    @endphp
-                @elseif($enquiry->status == \Botble\Ecommerce\Enums\EnquiryStatusEnum::CONTACTED)
-                    @php
-                        $class= "success";
-                    @endphp
-                @elseif($enquiry->status == \Botble\Ecommerce\Enums\EnquiryStatusEnum::NOTAVAILABLE)
-                    @php
-                        $class= "warning";
-                    @endphp
-                @else
-                    @php
-                        $class= "secondary";
-                    @endphp
-                @endif
-                <span class="badge bg-{{$class}} text-{{$class}}-fg d-flex align-items-center gap-1">
-
-                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <path d="M4 19a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
-                        <path d="M11.5 17h-5.5v-14h-2"></path>
-                        <path d="M6 5l14 1l-1 7h-13"></path>
-                        <path d="M15 19l2 2l4 -4"></path>
-                    </svg> {{ $enquiry->status->label() }}
-                </span>
-
-                <div class="row">
-                    <div class="wrapper-content mb20">
-                        <div class="next-card-section p-none-b">
-                            <div class="flexbox-grid-default flexbox-align-items-center">
-                                <div class="flexbox-auto-content-left">
-                                    <label class="title-product-main text-no-bold">{{ trans('plugins/ecommerce::enquiry.information') }}</label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="next-card-section border-none-t">
-                            <div class="mb5">
-                                <strong class="text-capitalize">{{ $enquiry->name }}</strong>
-                            </div>
-                            <ul class="ws-nm text-infor-subdued">
-                                <li class="overflow-ellipsis">
-                                    <span><i class="fa fa-envelope cursor-pointer mr5"></i></span><a class="hover-underline" href="mailto:{{ $enquiry->email  }}">{{ $enquiry->email  }}</a>
-                                </li>
-                                <li class="overflow-ellipsis"><span><i class="fa fa-phone-square cursor-pointer mr5"></i></span><a class="hover-underline" href="tel:{{ $enquiry->phone  }}">{{ $enquiry->phone  }}</a></li>
-                                <li class="overflow-ellipsis"><span><i class="fa fa-address-book cursor-pointer mr5"></i></span>Address : {{ $enquiry->address  }}</li>
-                                <li class="overflow-ellipsis"><span><i class="fa fa-map-marker cursor-pointer mr5"></i></span>City : {{ $enquiry->cityName->name  }}</li>
-                                <li class="overflow-ellipsis"><span><i class="fa fa-globe cursor-pointer mr5"></i></span>State : {{ $enquiry->stateName->name  }}</li>
-                                <li class="overflow-ellipsis"><span><i class="fa fa-code cursor-pointer mr5"></i></span>Zipcode : {{ $enquiry->zip_code  }}</li>
-                                <li class="overflow-ellipsis"><span><i class="fa fa-paperclip cursor-pointer mr5"></i></span><a href="{{ RvMedia::getImageUrl($enquiry->attachment, 'small', false, RvMedia::getDefaultImage()) }}" download=""><i class="fa fa-download"></i> Attechment</a></li>
-                                <li class="overflow-ellipsis"></li>
-                                <li class="overflow-ellipsis">
-                                    <a href="{{ route('enquires.contacted',$enquiry->id) }}" class="btn btn-success">{{ trans('plugins/ecommerce::enquiry.statuses.contacted') }}</a>
-                                    <a href="{{ route('enquires.not_available',$enquiry->id) }}" class="btn btn-warning">{{ trans('plugins/ecommerce::enquiry.statuses.not_available') }}</a>
-                                    <a href="{{ route('enquires.rejected',$enquiry->id) }}" class="btn btn-danger">{{ trans('plugins/ecommerce::enquiry.statuses.rejected') }}</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="wrapper-content bg-gray-white mb20">
-                        <div class="pd-all-20">
-                            <div class="p-b10">
-                                <strong>{{ trans('plugins/ecommerce::enquiry.status') }} </strong>
-                            </div>
-                            <br>
-                            {!! $enquiry->status->toHtml() !!}
-                        </div>
-                    </div>
-                    @if ($enquiry->product)
-                        <div class="wrapper-content bg-gray-white mb20">
-                            <div class="pd-all-20">
-                                <div class="p-b10">
-                                    <strong>{{ trans('plugins/ecommerce::products.product_name') }}</strong>
-                                    <ul class="p-sm-r mb-0">
-                                        <li class="ws-nm">
-                                            <a href="{{ route('products.edit', $enquiry->product_id) }}" class="ww-bw text-no-bold" target="_blank">{{ $enquiry->product->name }}</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    @endif
-                    @if (is_plugin_active('marketplace') && $enquiry->product->store->name)
-                    <div class="wrapper-content bg-gray-white mb20">
-                        <div class="pd-all-20">
-                            <div class="p-b10">
-                                <strong>{{ trans('plugins/marketplace::store.store') }}</strong>
-                                <ul class="p-sm-r mb-0">
-                                    <li class="ws-nm">
-                                        <a href="{{ $enquiry->product->store->url }}" class="ww-bw text-no-bold" target="_blank">{{ $enquiry->product->store->name }}</a>
-                                        @if($enquiry->product->store->is_verified)
-                                        <img class="verified-store-main" style="width: 20px;" src="{{ asset('/storage/stores/verified.png')}}" alt="Verified">
+    <div id="main-order-content">
+        <div class="row row-cards">
+            <div class="col-md-12">
+                <x-core::card class="mb-3">
+                    <x-core::card.header class="justify-content-between">
+                        <x-core::card.title>
+                            {{ trans('plugins/ecommerce::enquiry.enquiry_information') }} {{ $enquiry->code }}
+                        </x-core::card.title>
+                        @if ($enquiry->status == \Botble\Ecommerce\Enums\EnquiryStatusEnum::REJECT)
+                            <x-core::badge color="danger" class="d-flex align-items-center gap-1">
+                                <x-core::icon name="ti ti-circle-x"></x-core::icon>
+                                {{ trans('plugins/ecommerce::enquiry.statuses.rejected') }}
+                            </x-core::badge>
+                        @elseif($enquiry->status == \Botble\Ecommerce\Enums\EnquiryStatusEnum::CONTACTED)
+                            <x-core::badge color="success" class="d-flex align-items-center gap-1">
+                                <x-core::icon name="ti ti-circle-check"></x-core::icon>
+                                {{ trans('plugins/ecommerce::enquiry.statuses.contacted') }}
+                            </x-core::badge>
+                        @elseif($enquiry->status == \Botble\Ecommerce\Enums\EnquiryStatusEnum::NOTAVAILABLE)
+                            <x-core::badge color="warning" class="d-flex align-items-center gap-1">
+                                <x-core::icon name="ti ti-circle-off"></x-core::icon>
+                                {{ trans('plugins/ecommerce::enquiry.statuses.not_available') }}
+                            </x-core::badge>
+                        @else
+                            <x-core::badge color="info" class="d-flex align-items-center gap-1">
+                                <x-core::icon name="ti ti-alert-circle"></x-core::icon>
+                                {{ trans('plugins/ecommerce::enquiry.statuses.pending') }}
+                            </x-core::badge>
+                        @endif
+                    </x-core::card.header>
+                    <x-core::card.body>
+                        <x-core::table :hover="false" :striped="false" class="table-borderless">
+                            <x-core::table.body>
+                                <x-core::table.body.row>
+                                    <x-core::table.body.cell>
+                                        <strong>{{ trans('plugins/ecommerce::enquiry.customer_name') }}</strong>
+                                    </x-core::table.body.cell>
+                                    <x-core::table.body.cell>
+                                        <x-core::icon name="ti ti-user" /> {{ $enquiry->name }}
+                                    </x-core::table.body.cell>
+                                </x-core::table.body.row>
+                                <x-core::table.body.row>
+                                    <x-core::table.body.cell>
+                                        <strong>{{ trans('plugins/ecommerce::enquiry.customer_email') }}</strong>
+                                    </x-core::table.body.cell>
+                                    <x-core::table.body.cell>
+                                        <a class="hover-underline" href="mailto:{{ $enquiry->email  }}"><x-core::icon name="ti ti-mail" /> {{ $enquiry->email  }}</a>
+                                    </x-core::table.body.cell>
+                                </x-core::table.body.row>
+                                <x-core::table.body.row>
+                                    <x-core::table.body.cell>
+                                        <strong>{{ trans('plugins/ecommerce::enquiry.customer_phone') }}</strong>
+                                    </x-core::table.body.cell>
+                                    <x-core::table.body.cell>
+                                        <a class="hover-underline" href="tel:{{ $enquiry->phone  }}"><x-core::icon name="ti ti-phone" /> {{ $enquiry->phone  }}</a>
+                                    </x-core::table.body.cell>
+                                </x-core::table.body.row>
+                                <x-core::table.body.row>
+                                    <x-core::table.body.cell>
+                                        <strong>{{ trans('plugins/ecommerce::enquiry.customer_address') }}</strong>
+                                    </x-core::table.body.cell>
+                                    <x-core::table.body.cell>
+                                        <address class="mb-0">{{ $enquiry->address.', '.$enquiry->cityName->name.' - '.$enquiry->zip_code.', '.$enquiry->stateName->name }}</address>
+                                    </x-core::table.body.cell>
+                                </x-core::table.body.row>
+                                <x-core::table.body.row>
+                                    <x-core::table.body.cell>
+                                        <strong>{{ trans('plugins/ecommerce::enquiry.status') }}</strong>
+                                    </x-core::table.body.cell>
+                                    <x-core::table.body.cell>
+                                        @if ($enquiry->status == \Botble\Ecommerce\Enums\EnquiryStatusEnum::REJECT)
+                                            <x-core::badge color="danger">
+                                                <x-core::icon name="ti ti-circle-x"></x-core::icon>
+                                                {{ trans('plugins/ecommerce::enquiry.statuses.rejected') }}
+                                            </x-core::badge>
+                                        @elseif($enquiry->status == \Botble\Ecommerce\Enums\EnquiryStatusEnum::CONTACTED)
+                                            <x-core::badge color="success">
+                                                <x-core::icon name="ti ti-circle-check"></x-core::icon>
+                                                {{ trans('plugins/ecommerce::enquiry.statuses.contacted') }}
+                                            </x-core::badge>
+                                        @elseif($enquiry->status == \Botble\Ecommerce\Enums\EnquiryStatusEnum::NOTAVAILABLE)
+                                            <x-core::badge color="warning">
+                                                <x-core::icon name="ti ti-circle-off"></x-core::icon>
+                                                {{ trans('plugins/ecommerce::enquiry.statuses.not_available') }}
+                                            </x-core::badge>
+                                        @else
+                                            <x-core::badge color="info">
+                                                <x-core::icon name="ti ti-alert-circle"></x-core::icon>
+                                                {{ trans('plugins/ecommerce::enquiry.statuses.pending') }}
+                                            </x-core::badge>
                                         @endif
-                                        <small class="badge bg-warning text-white">{{ $enquiry->product->store->shop_category->label() }}</small>
-                                    </li>
-                                </ul>
-                            </div>
+                                    </x-core::table.body.cell>
+                                </x-core::table.body.row>
+                                @if(RvMedia::getImageUrl($enquiry->attachment))
+                                <x-core::table.body.row>
+                                    <x-core::table.body.cell>
+                                        <strong>{{ trans('plugins/ecommerce::enquiry.customer_attachment') }}</strong>
+                                    </x-core::table.body.cell>
+                                    <x-core::table.body.cell>
+                                        <a href="{{ RvMedia::getImageUrl($enquiry->attachment, 'small', false, RvMedia::getDefaultImage()) }}" download=""><x-core::icon name="ti ti-paperclip" /> {{ trans('plugins/ecommerce::enquiry.download_attachment') }}</a>
+                                    </x-core::table.body.cell>
+                                </x-core::table.body.row>
+                                @endif
+                                @if ($enquiry->product)
+                                    <x-core::table.body.row>
+                                        <x-core::table.body.cell>
+                                            <strong>{{ trans('plugins/ecommerce::enquiry.customer_product') }}</strong>
+                                        </x-core::table.body.cell>
+                                        <x-core::table.body.cell>
+                                            <a href="{{ route('products.edit', $enquiry->product_id) }}" class="ww-bw text-no-bold" target="_blank"><x-core::icon name="ti ti-external-link" /> {{ $enquiry->product->name }}</a>
+                                        </x-core::table.body.cell>
+                                    </x-core::table.body.row>
+                                @endif
+                                @if (is_plugin_active('marketplace') && $enquiry->product->store->name)
+                                    <x-core::table.body.row>
+                                        <x-core::table.body.cell>
+                                            <strong>{{ trans('plugins/ecommerce::enquiry.customer_store') }}</strong>
+                                        </x-core::table.body.cell>
+                                        <x-core::table.body.cell>
+                                            <a href="{{ $enquiry->product->store->url }}" class="ww-bw text-no-bold" target="_blank"><x-core::icon name="ti ti-external-link" /> {{ $enquiry->product->store->name }}</a>
+                                            @if($enquiry->product->store->is_verified)
+                                                <img class="verified-store-main" style="width: 20px;" src="{{ asset('/storage/stores/verified.png')}}" alt="Verified">
+                                            @endif
+                                            <small class="badge bg-warning text-white">{{ $enquiry->product->store->shop_category->label() }}</small>
+                                        </x-core::table.body.cell>
+                                    </x-core::table.body.row>
+                                @endif
+                            </x-core::table.body>
+                        </x-core::table>
+                    </x-core::card.body>
+                    <x-core::card.footer>
+                        <div class="btn-list">
+                            <x-core::button tag="a" icon="ti ti-circle-check" class="btn btn-success" :href="route('enquires.contacted', ['id' => $enquiry->id])">
+                                {{ trans('plugins/ecommerce::enquiry.statuses.contacted') }}
+                            </x-core::button>
+                            <x-core::button tag="a" icon="ti ti-circle-off" class="btn btn-warning" :href="route('enquires.not_available', ['id' => $enquiry->id])">
+                                {{ trans('plugins/ecommerce::enquiry.statuses.not_available') }}
+                            </x-core::button>
+                            <x-core::button tag="a" icon="ti ti-circle-x" class="btn btn-danger" :href="route('enquires.rejected', ['id' => $enquiry->id])">
+                                {{ trans('plugins/ecommerce::enquiry.statuses.reject') }}
+                            </x-core::button>
                         </div>
-                    </div>
-                    @endif
-                </div>
+                    </x-core::card.footer>
+                </x-core::card>
             </div>
         </div>
     </div>
-</div>
 @endsection
