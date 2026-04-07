607419804e1f7d7f7fbf16c7384ca89e740ea244
Deepak K Soni <digital.sparrowaves@gmail.com>
2026-04-07 18:05:50 +0530

    Update header.blade.php

--- platform/themes/farmart/partials/header.blade.php
+++ platform/themes/farmart/partials/header.blade.php
@@ -157,49 +157,13 @@
                     <div class="header-items header__left">
                         <div class="logo">
                             <a href="{{ BaseHelper::getHomepageUrl() }}">
-                                {!! Theme::getLogoImage() !!}
+                                {!! Theme::getLogoImage(['style' => 'max-height: 45px']) !!}
                             </a>
                         </div>
                     </div>
                     <div class="header-items header__center">
                         @if (is_plugin_active('ecommerce'))
-                            <x-plugins-ecommerce::fronts.ajax-search class="form--quick-search">
-                                <div
-                                    class="form-group--icon"
-                                    style="display: none"
-                                >
-                                    <div class="product-category-label">
-                                        <label for="product-category-select" class="text">{{ __('All Categories') }}</label>
-                                        <span class="svg-icon">
-                                            <svg>
-                                                <use
-                                                    href="#svg-icon-chevron-down"
-                                                    xlink:href="#svg-icon-chevron-down"
-                                                ></use>
-                                            </svg>
-                                        </span>
-                                    </div>
-                                    <x-plugins-ecommerce::fronts.ajax-search.categories-dropdown
-                                        class="form-control product-category-select"
-                                        id="product-category-select"
-                                    />
-                                </div>
-                                <x-plugins-ecommerce::fronts.ajax-search.input type="text" class="form-control input-search-product" />
-                                <button
-                                    class="btn"
-                                    type="submit"
-                                    aria-label="Submit"
-                                >
-                                    <span class="svg-icon">
-                                        <svg>
-                                            <use
-                                                href="#svg-icon-search"
-                                                xlink:href="#svg-icon-search"
-                                            ></use>
-                                        </svg>
-                                    </span>
-                                </button>
-                            </x-plugins-ecommerce::fronts.ajax-search>
+                            {!! Theme::partial('header-search-ecommerce') !!}
                         @endif
                     </div>
                     <div class="header-items header__right">
