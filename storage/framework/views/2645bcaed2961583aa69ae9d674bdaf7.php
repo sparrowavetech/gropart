c5409f7b051e90d4e0cef7345d0e1e4778e4d31e
Deepak K Soni <digital.sparrowaves@gmail.com>
2026-05-29 09:58:42 +0530

    ignore

---
+++ storage/framework/views/2645bcaed2961583aa69ae9d674bdaf7.php
@@ -0,0 +1,7 @@
+<thead <?php echo e($attributes); ?>>
+    <tr>
+        <?php echo e($slot); ?>
+
+    </tr>
+</thead>
+<?php /**PATH D:\xampp82\htdocs\gropart\platform\core\base\/resources/views/components/table/header/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/9d5c020acab6e3125d0c5a4eae40c7b8.php
@@ -0,0 +1,5 @@
+<th <?php echo e($attributes); ?>>
+    <?php echo e($slot); ?>
+
+</th>
+<?php /**PATH D:\xampp82\htdocs\gropart\platform\core\base\/resources/views/components/table/header/cell.blade.php ENDPATH**/ ?>
