ed646e75eecc66d90c74a50390b392c8ebdffa9a
Deepak K Soni <digital.sparrowaves@gmail.com>
2026-08-16 14:28:58 +0530

    SMS Gateway Plugin changes

--- platform/plugins/sms/resources/views/settings.blade.php
+++ platform/plugins/sms/resources/views/settings.blade.php
@@ -1,48 +1,41 @@
 @extends(BaseHelper::getAdminMasterLayoutTemplate())
-@section('content')
-{!! Form::open(['url' => route('sms.settings'), 'class' => 'main-setting-form']) !!}
-<div class="flexbox-annotated-section">
-    <div class="flexbox-annotated-section-annotation">
-        <div class="annotated-section-title pd-all-20">
-            <h2>{{ trans('plugins/sms::sms.settings.title') }}</h2>
-        </div>
-        <div class="annotated-section-description pd-all-20 p-none-t">
-            <p class="color-note">{{ trans('plugins/sms::sms.settings.description') }}</p>
-        </div>
-    </div>
 
-    <div class="flexbox-annotated-section-content">
-        <div class="wrapper-content pd-all-20">
-            <div class="form-group mb-3">
+@section('content')
+    {!! Form::open(['url' => route('sms.settings'), 'class' => 'main-setting-form']) !!}
+        <x-core-setting::section
+            :title="trans('plugins/sms::sms.settings.title')"
+            :description="trans('plugins/sms::sms.settings.description')"
+        >
+            <x-core-setting::form-group>
                 <label class="text-title-field" for="sms_url">{{ trans('plugins/sms::sms.settings.sms_url') }}</label>
                 <input data-counter="500" type="text" class="next-input" name="sms_url" id="sms_url" value="{{ setting('sms_url') }}" placeholder="{{ trans('plugins/sms::sms.settings.sms_url') }}">
-                {{ Form::helper(trans('plugins/sms::sms.settings.sms_url_helper')) }}
-            </div>
-        </div>
-    </div>
+                <div class="mt-2 text-muted" style="font-size: 0.85rem; line-height: 1.5;">
+                    <p class="mb-1"><strong>{{ __('Example API URL:') }}</strong></p>
+                    <pre class="bg-light p-2 border rounded" style="word-break: break-all; white-space: pre-wrap; font-size: 0.8rem; color: #555;">{{ trans('plugins/sms::sms.settings.sms_url_helper') }}</pre>
+                    <p class="mb-0 mt-2"><strong>{{ __('Supported Placeholders:') }}</strong></p>
+                    <ul class="ps-3 mb-0" style="list-style-type: disc;">
+                        <li><code>@{{mobile}}</code> - {{ __('Recipient mobile number') }}</li>
+                        <li><code>@{{message}}</code> - {{ __('SMS content message text') }}</li>
+                        <li><code>@{{template_id}}</code> - {{ __('DLT Template ID (if applicable)') }}</li>
+                    </ul>
+                </div>
+            </x-core-setting::form-group>
 
-    <div class="flexbox-annotated-section-content">
-        <div class="wrapper-content pd-all-20">
-            <div class="form-group mb-3">
-                <label class="text-title-field" for="sms_otp_enabled">{{ trans('plugins/sms::sms.sms_otp_enabled') }}
-                </label>
-                <label class="me-2">
-                    <input type="radio" name="sms_otp_enabled" value="1" @if (setting('sms_otp_enabled')) checked @endif>{{ trans('core/setting::setting.general.yes') }}
-                </label>
-                <label>
-                    <input type="radio" name="sms_otp_enabled" value="0" @if (!setting('sms_otp_enabled')) checked @endif>{{ trans('core/setting::setting.general.no') }}
-                </label>
+            <input type="hidden" name="sms_otp_enabled" value="0">
+            <x-core-setting::on-off
+                name="sms_otp_enabled"
+                :label="trans('plugins/sms::sms.sms_otp_enabled')"
+                :value="setting('sms_otp_enabled')"
+            />
+        </x-core-setting::section>
+
+        <div class="flexbox-annotated-section" style="border: none">
+            <div class="flexbox-annotated-section-annotation">
+                &nbsp;
+            </div>
+            <div class="flexbox-annotated-section-content">
+                <button class="btn btn-success" type="submit">{{ trans('plugins/sms::sms.save_settings') }}</button>
             </div>
         </div>
-    </div>
-</div>
-<div class="flexbox-annotated-section" style="border: none">
-    <div class="flexbox-annotated-section-annotation">
-        &nbsp;
-    </div>
-    <div class="flexbox-annotated-section-content">
-        <button class="btn btn-info" type="submit">{{ trans('plugins/sms::sms.save_settings') }}</button>
-    </div>
-</div>
-{!! Form::close() !!}
-@endsection
+    {!! Form::close() !!}
+@stop
