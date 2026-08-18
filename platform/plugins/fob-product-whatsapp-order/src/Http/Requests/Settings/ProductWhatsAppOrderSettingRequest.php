b5387ba229c249675e2532a4f44b44759eb73952
Deepak K Soni <digital.sparrowaves@gmail.com>
2026-08-19 00:37:35 +0530

    Whatsapp order plugin bug issues fixed

--- platform/plugins/fob-product-whatsapp-order/src/Http/Requests/Settings/ProductWhatsAppOrderSettingRequest.php
+++ platform/plugins/fob-product-whatsapp-order/src/Http/Requests/Settings/ProductWhatsAppOrderSettingRequest.php
@@ -2,6 +2,7 @@
 
 namespace FriendsOfBotble\ProductWhatsAppOrder\Http\Requests\Settings;
 
+use Botble\Base\Rules\OnOffRule;
 use Botble\Support\Http\Requests\Request;
 
 class ProductWhatsAppOrderSettingRequest extends Request
@@ -9,9 +10,15 @@ class ProductWhatsAppOrderSettingRequest extends Request
     public function rules(): array
     {
         return [
-            'product_whatsapp_order_phone_number' => ['nullable', 'string', 'regex:/^[+]?[0-9\s\-()]+$/'],
-            'product_whatsapp_order_message_template' => ['nullable', 'string', 'max:1000'],
+            'product_whatsapp_order_enabled' => [new OnOffRule()],
+            'product_whatsapp_order_phone_number' => ['nullable', 'string', 'max:60'],
+            'product_whatsapp_order_message_template' => ['nullable', 'string', 'max:2000'],
+            'product_whatsapp_order_button_icon' => ['nullable', 'string', 'max:120'],
+            'product_whatsapp_order_button_color' => ['nullable', 'string', 'max:50'],
+            'product_whatsapp_order_button_hover_color' => ['nullable', 'string', 'max:50'],
             'product_whatsapp_order_button_radius' => ['nullable', 'numeric', 'min:0', 'max:50'],
+            'product_whatsapp_order_show_for_out_of_stock' => [new OnOffRule()],
+            'product_whatsapp_order_show_always' => [new OnOffRule()],
         ];
     }
 }
